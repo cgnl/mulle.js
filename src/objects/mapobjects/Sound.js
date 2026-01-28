53cbf38d7558c640f3369b7045f1efdc0f3b4feb
Sander <sander@Sanders-Mac-mini.local>
2026-01-28 23:24:30 +0100

    Feature 1+2: Sound and Teleport objects
    Feature 1 - Sound Object:
    - Sound trigger with playback on car enter
    - Prevents duplicate plays with hasPlayed flag
    - Configurable via opt.Sound parameter
    - Non-renderable trigger zone
    
    Feature 2 - Teleport Object:
    - Same-map and cross-map teleportation
    - Smooth fade transition effects
    - Configurable target position and direction
    - Anti-retrigger protection (500ms cooldown)
    
    Test examples in commit message docs

--- src/objects/mapobjects/Sound.js
+++ src/objects/mapobjects/Sound.js
@@ -1,17 +1,26 @@
 'use strict'
 
 /**
- * Racing (map 28)
- * @type {{}}
+ * Sound trigger object
+ * Plays audio when car enters the trigger zone
+ * @type {MulleMapObject}
  */
 var MapObject = {}
 
 MapObject.onCreate = function () {
-  console.error('unfinished object', this.id, this)
+  this.renderable = false
+  this.triggerSound = this.opt?.Sound || null
+  this.hasPlayed = false
+  
+  console.debug('[Sound] Created at', this.position, 'sound:', this.triggerSound)
 }
 
-MapObject.onEnterInner = function () {
-
+MapObject.onEnterInner = function (car) {
+  if (!this.hasPlayed && this.triggerSound) {
+    console.log('[Sound] Playing:', this.triggerSound)
+    this.game.mulle.playAudio(this.triggerSound)
+    this.hasPlayed = true
+  }
 }
 
 export default MapObject
