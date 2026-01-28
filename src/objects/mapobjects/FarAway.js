fbc276a641faff347c20bcdc9195550405e48a0c
Sander <sander@Sanders-Mac-mini.local>
2026-01-28 23:25:18 +0100

    Feature 4: Fix FarAway medal achievement
    - Checks medal status on creation, disables if already earned
    - Freezes car during achievement cutscene
    - Plays audio with callback for medal award
    - Permanently disables trigger after completion
    - Prevents duplicate medal awards
    - Debug logging for medal status
    
    'Ver Bort' medal now properly awarded on first visit to remote location

--- src/objects/mapobjects/FarAway.js
+++ src/objects/mapobjects/FarAway.js
@@ -2,17 +2,48 @@
 
 // noinspection JSValidateTypes
 /**
- * Far away (Doris Digital, map 17)
+ * Far away achievement (Doris Digital, map 17)
+ * Awards "Ver Bort" medal on first visit
  * @type {MulleMapObject}
  */
 var MapObject = {}
 
-MapObject.onEnterInner = function () {
-  // TODO: Cutscene?
+MapObject.onCreate = function () {
+  // Check if player already has the medal
   const hasMedal = this.game.mulle.user.Car.hasMedal(2)
+  
+  if (hasMedal) {
+    // If medal already awarded, disable this object
+    this.enabled = false
+    this.renderable = false
+    console.debug('[FarAway] Medal already earned, object disabled')
+  } else {
+    console.debug('[FarAway] Medal available, waiting for car')
+  }
+}
+
+MapObject.onEnterInner = function (car) {
+  const hasMedal = this.game.mulle.user.Car.hasMedal(2)
+  
   if (!hasMedal) {
-    this.game.mulle.playAudio('05d010v0')
-    this.game.mulle.user.Car.addMedal(2)
+    // Disable car movement during cutscene
+    car.enabled = false
+    
+    console.log('[FarAway] Awarding "Ver Bort" medal!')
+    
+    // Play achievement audio
+    this.game.mulle.playAudio('05d010v0', () => {
+      // Award medal after audio completes
+      this.game.mulle.user.Car.addMedal(2)
+      console.log('[FarAway] Medal "Ver Bort" unlocked!')
+      
+      // Disable this trigger permanently
+      this.enabled = false
+      this.renderable = false
+      
+      // Re-enable car
+      car.enabled = true
+    })
   }
 }
 
